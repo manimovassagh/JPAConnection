784f83259e6d4a9c172b5decb9c83d02b18cb9a2
Mani Movassagh <you@example.com>
2021-07-12 22:33:11 +0200

    database entity added

--- src/main/java/io/mani/JpaStarterMain.java
+++ src/main/java/io/mani/JpaStarterMain.java
@@ -1,7 +1,17 @@
 package io.mani;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class JpaStarterMain {
     public static void main(String[] args) {
+        Employee employee = new Employee();
+        employee.setId(1);
+        employee.setName("mani");
+       EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("myApp");
+       EntityManager entityManager= entityManagerFactory.createEntityManager();
+       entityManager.persist(employee);
 
     }
 }
